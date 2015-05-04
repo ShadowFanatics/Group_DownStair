cdcf04894fe61630946f8c7edc9f2327bd9429b1
ShadowFanatics <inin610719@gmail.com>
2015-05-04 21:44:22 +0800

    修改架構
    修改了物理判定
    斜面有寫了 只是怪怪的  先用平面就好QQ

--- src/com/group_downstair/main/GameSceneActivity.java
+++ src/com/group_downstair/main/GameSceneActivity.java
@@ -32,106 +32,153 @@ import android.view.View;
 
 public class GameSceneActivity extends Activity {
 	private View myPanel;
-	private Timer gameTimer;
 	private AnimateObject player;
-	private ArrayList<AnimateObject> stairs = new ArrayList<AnimateObject>();
+	private ArrayList<StairObject> stairs = new ArrayList<StairObject>();
 	private int screenWidth;
 	private int screenHeight;
 	private Resources res;
 	private float lastStairY = 100;
-	private final float lengthBetweenStair = 100;
-	private boolean playerFall = true;
+	private int lastFloor = 0;
+	private final float lengthBetweenStair = 150;
+	private Thread paintThread;
+	private final long fps = 60;
+	private boolean gameOver = false;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// 用來取得螢幕大小
 		DisplayMetrics displayMetrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-		//preferSize = displayMetrics.widthPixels / sudokuSize;
+
 		screenWidth = displayMetrics.widthPixels;
 		screenHeight = displayMetrics.heightPixels;
 		myPanel = new Panel(this);
 		setContentView(myPanel);
+
+	}
+
+	public void gameRun() {
+
+		Physical.runObjects2();
 		
-		gameTimer = new Timer();
-		gameTimer.schedule(timer_task, 0, 20);
-	}
-	
-	public void run() {
-		if ( playerFall ) {
-			player.move(0, 5);
+		if (player.getY() > screenHeight ) {
+			//gameOver = true;
+			player.setLocation(player.getX(), 0);
 		}
+		if (player.getX() < 0 ) {
+			player.setSpeedX((float) (-player.getSpeedX()));
+		}
+		if (player.getX() > screenWidth - player.getWidth() ) {
+			player.setSpeedX((float) (-player.getSpeedX()));
+		}
+		
 		float max = 0;
-		int outScreenIndex = -1;
+		int index = -1;
 		for (int i = 0; i < stairs.size(); i++) {
-			stairs.get(i).move(0, -1);
-			if ( Physical.isCollide(stairs.get(i),player) ) {
-				player.setLocation(player.getX(), stairs.get(i).getY()-player.getHeight());
+			if (stairs.get(i).getButtom() > max) {
+				max = stairs.get(i).getY();
 			}
-			float tempY = stairs.get(i).getY();
-			if ( tempY > max ) {
-				max = tempY;
+			if (stairs.get(i).getY() < -20) {
+				index = i;
 			}
-			if( stairs.get(i).getY() < -20 ) {
-				outScreenIndex = i;
-			}
-        }
-		if ( outScreenIndex != -1 ) {
+		}
+		if (index != -1) {
 			lastStairY = max + lengthBetweenStair;
-			stairs.get(outScreenIndex).setLocation(getRandomInt(0, screenWidth - 100), lastStairY);
+			stairs.get(index).setLocation(getRandomInt(0, screenWidth - 100),
+					lastStairY);
+			//stairs.get(index).setDegree(getRandomInt(-45, 45));
+			stairs.get(index).setFloor(lastFloor);
+			lastFloor++;
 		}
 	}
-	
-	class Panel extends View {
+
+	class Panel extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+		private SurfaceHolder surfaceHolder;
+
 		public Panel(Context context) {
 			super(context);
 			res = getResources();
-			player = new AnimateObject(res, Image.player, screenWidth/2, 0);
-			for ( int i = 0 ; i < 10 ; i++ ) {
+			surfaceHolder = this.getHolder();
+			surfaceHolder.addCallback(this);
+			paintThread = new Thread(this);
+
+			player = new AnimateObject(res, Image.player, screenWidth / 2, 0);
+			player.setGravity(true);
+			Physical.addObject(player);
+			for (int i = 0; i < 10; i++) {
 				lastStairY += lengthBetweenStair;
-				stairs.add(new AnimateObject(res, Image.stair, getRandomInt(0, screenWidth - 100), lastStairY));
+				StairObject temp = new StairObject(res, Image.stair, getRandomInt(0,
+						screenWidth - 100), lastStairY, lastFloor);
+				temp.addSpeedY(-1);
+				stairs.add(temp);
+				Physical.addObject(temp);
+				lastFloor++;
 			}
 		}
 
-		public void onDraw(Canvas canvas) {
-			canvas.drawColor(Color.WHITE);
-			canvas.drawBitmap(player.getImg(), player.getMatrix(), null);
-			for (int i = 0; i < stairs.size(); i++) {
-				canvas.drawBitmap(stairs.get(i).getImg(), stairs.get(i).getMatrix(), null);
-	        }
-			//canvas.drawBitmap(test.getImg(), test.getMatrix(), null);
+		public void draw() {
+			Canvas canvas = null;
+			try {
+				canvas = surfaceHolder.lockCanvas(null);
+				synchronized (surfaceHolder) {
+					canvas.drawColor(Color.BLUE);
+					canvas.drawBitmap(player.getImg(), player.getMatrix(), null);
+					for (int i = 0; i < stairs.size(); i++) {
+						canvas.drawBitmap(stairs.get(i).getImg(), stairs.get(i)
+								.getMatrix(), null);
+					}
+				}
+			} finally {
+				if (canvas != null) {
+					surfaceHolder.unlockCanvasAndPost(canvas);
+				}
+			}
+
 		}
 
+		public void run() {
+			while (!gameOver) {
+				gameRun();
+				draw();
+				try {
+					Thread.sleep(17);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					Log.e("thread", e.toString());
+				}
+			}
+		}
+
+		public void surfaceCreated(SurfaceHolder holder) {
+			paintThread.start();
+		}
+
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+		}
+
+		public void surfaceDestroyed(SurfaceHolder holder) {
+		}
 	}
-	
-	public boolean onKeyDown(int keyCode,KeyEvent event){
-	      if(keyCode==KeyEvent.KEYCODE_BACK && event.getRepeatCount()==0){   //確定按下退出鍵and防止重複按下退出鍵
-	    	  finish();
-	      }
-	      return false;
+
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) { // 確定按下退出鍵and防止重複按下退出鍵
+			finish();
+		}
+		if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) { // 確定按下退出鍵and防止重複按下退出鍵
+			player.setSpeedX(-5);
+		}
+		if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) { // 確定按下退出鍵and防止重複按下退出鍵
+			player.setSpeedX(5);
+		}
+		return false;
 	}
-	
+
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	private TimerTask timer_task = new TimerTask() {
-		public void run() {
-			Message msg = new Message();
-			msg.what = 1;
-			timer_handler.sendMessage(msg);
-		}
-	};
 
-	private Handler timer_handler = new Handler() {
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			run();
-			myPanel.invalidate();
-		}
-	};
-	
 	private int getRandomInt(int min, int max) {
 		Random r = new Random();
 		return r.nextInt(max - min + 1) + min;
--- src/engine/Physical.java
+++ src/engine/Physical.java
@@ -1,31 +1,99 @@
 package engine;
 
+import java.util.ArrayList;
+
+import android.util.Log;
 import objects.*;
 
 public class Physical {
+	private static float gravity = (float) 0.098;
+	private static float reflect = (float) 0.7;
+	private static ArrayList<AnimateObject> objects = new ArrayList<AnimateObject>();
+
+	public static void runObjects2() {
+		for (int i = 0; i < objects.size(); i++) {
+			AnimateObject temp = objects.get(i);
+			if ( temp.isGravity() ) {
+				if (temp.getSpeedY() < 5 ) {
+					temp.addSpeedY(gravity);
+				}
+			}
+			temp.move(temp.getSpeedX(), temp.getSpeedY());
+		}
+		AnimateObject player = objects.get(0);
+		for (int i = 1; i < objects.size(); i++) {
+			AnimateObject temp = objects.get(i);
+			if ( isCollide(temp, player) ) {
+				player.move(0-player.getSpeedX(), 0-player.getSpeedY());
+				player.addSpeedY(0-gravity);
+				float reflectSpeed = (float) (0-player.getSpeedY())*reflect;
+				/*if ( player.getSpeedY() < 1 && player.getSpeedY() > -1 ) {
+					reflectSpeed = -1;
+				}*/
+				player.setSpeedY(reflectSpeed);
+				
+				Log.e("setSpeedY",String.valueOf(player.getSpeedY()));
+				player.move(player.getSpeedX(), player.getSpeedY()-1);
+			}
+		}
+	}
+	
+	public static void runObjects() {
+		for (int i = 0; i < objects.size(); i++) {
+			AnimateObject temp = objects.get(i);
+			if ( temp.isGravity() ) {
+				if (temp.getSpeedY() < 5 ) {
+					temp.addSpeedY(gravity);
+				}
+			}
+			
+			temp.move(temp.getSpeedX(), temp.getSpeedY());
+		}
+		AnimateObject player = objects.get(0);
+		double playerAngle = Math.atan2(player.getSpeedY(), player.getSpeedX());
+		for (int i = 1; i < objects.size(); i++) {
+			AnimateObject temp = objects.get(i);
+			if ( isCollide(temp, player) ) {
+				player.move(0, 0-player.getSpeedY());
+				player.addSpeedY(0-gravity);
+				double objectAngle = (0-temp.getDegree())/180*Math.PI;
+				playerAngle -= Math.PI;
+				objectAngle += Math.PI/2;
+				double between = Math.PI - (playerAngle - objectAngle)*2;
+				double playerSpeed =  reflect * Math.sqrt(player.getSpeedX()*player.getSpeedX() + player.getSpeedY()*player.getSpeedY());
+				player.setSpeedX((float) (playerSpeed*Math.sin(between)));
+				player.setSpeedY((float) (playerSpeed*Math.cos(between)));
+				player.move(player.getSpeedX(), player.getSpeedY()-1);
+			}
+		}
+	}
+	
+	public static void addObject(AnimateObject object) {
+		objects.add(object);
+	}
+
 	public static boolean isCollide(AnimateObject A, AnimateObject B) {
 		int Ax = (int) A.getX();
 		int Ay = (int) A.getY();
 		int Aw = (int) A.getWidth();
-		int Ah = (int) A.getHeight();
+		int Ah = 1;
+		float Acos = A.getCos();
+		float Asin = A.getSin();
 		int Bx = (int) B.getX();
 		int By = (int) B.getY();
 		int Bw = (int) B.getWidth();
 		int Bh = (int) B.getHeight();
 		boolean collideX = false;
 		boolean collideY = false;
-		if (Ax < Bx + Bw && Ax > Bx) {
+		if (Ax < Bx + Bw && Bx < Ax + Aw * Acos) {
 			collideX = true;
 		}
-		if (Bx < Ax + Aw && Bx > Ax) {
-			collideX = true;
-		}
-		if (Ay < By + Bh && Ay > By) {
+		if (Ay + (Bx + Bw/2 - Ax) * Asin / Acos > By + Bh/2
+				&& By + Bh > Ay + (Bx + Bw/2 - Ax) * Asin / Acos) {
 			collideY = true;
 		}
-		if (By < Ay + Ah && By > Ay) {
-			collideY = true;
-		}
-		return (collideX&&collideY);
+		return (collideX && collideY);
 	}
+	
+	
 }
