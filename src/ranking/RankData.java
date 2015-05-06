5364f7a3455b8e3c59df18f0e24fa1f471d8b620
ccchhe <willian444@gmail.com>
2015-05-06 18:07:48 +0800

    登入 排行

---
+++ src/ranking/RankData.java
@@ -0,0 +1,46 @@
+package ranking;
+
+import android.graphics.drawable.Drawable;
+
+public class RankData {
+	private String name;
+	private String tsec;
+	private String date;
+	//private String time_string;
+	private Drawable icon1;
+	private Drawable icon2;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getTSec() {
+		return tsec;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public Drawable getIcon1(){
+		return icon1;
+	}
+	
+    public void setIcon1(Drawable icon) {
+        this.icon1 = icon;
+    }
+    
+	public Drawable getIcon2(){
+		return icon2;
+	}
+	
+    public void setIcon2(Drawable icon) {
+        this.icon2 = icon;
+    }
+	
+	public RankData(String tsec,String date, String name) {
+		this.tsec = tsec;
+		this.date = date;
+		this.name = name;
+	}
+}
